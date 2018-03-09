555ff53120e762caab80365910e4d30ad68524f9
Calvin Chueh <chchueh24@gmail.com>
2018-03-08 16:40:23 -0800

    Test Project

---
+++ CLLocationManager-Example/TestTest/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  TestTest
+//
+//  Created by Calvin Chueh on 3/5/18.
+//  Copyright © 2018 Calvin. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+
+
+
+
+// Flip this to *true* to use GCD
+
+private let kGCDMode: Bool = false
+
+
+
+
+class ViewController: UIViewController {
+
+  let locationManager = CLLocationManager()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    locationManager.pausesLocationUpdatesAutomatically = false
+    locationManager.allowsBackgroundLocationUpdates = true
+    locationManager.delegate = self
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    locationManager.requestAlwaysAuthorization()
+  }
+
+}
+
+
+extension ViewController: CLLocationManagerDelegate {
+  
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    locationManager.startUpdatingLocation()
+    locationManager.allowsBackgroundLocationUpdates = true
+    locationManager.startMonitoringSignificantLocationChanges()
+    print("available", CLLocationManager.significantLocationChangeMonitoringAvailable())
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    print(Date(),"UPDATING")
+    
+    if true == kGCDMode {
+    
+      DispatchQueue.main.asyncAfter(deadline: .now() + 30, execute: { [weak self] in
+        self?.locationManager.startUpdatingLocation()
+        self?.locationManager.allowsBackgroundLocationUpdates = true
+        self?.locationManager.startMonitoringSignificantLocationChanges()
+      })
+      
+    } else {
+      print(Date(), #function, "SPAMMING UPDATES")
+    }
+    
+    
+  }
+  
+}
